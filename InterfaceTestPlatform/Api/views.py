34fd64aba251197739fde28be28238b0ee2a2755
caozheng <469687182@qq.com>
2021-02-22 16:44:35 +0800

    完善功能

---
+++ InterfaceTestPlatform/Api/views.py
@@ -0,0 +1,23 @@
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+from . import *
+# Create your views here.
+from TestPlatform_User import models
+import json
+
+
+def get_pj_data(request):
+    pj_data = models.Project_Data.objects.all().values()
+    data_list = list(pj_data)
+    data_list = {"code": 0, "msg": "ok", "count": len(str(data_list)), "data": data_list}
+    return HttpResponse(json.dumps(data_list))
+
+
+def get_inter_data(request):
+    inter_data = models.Interface_Data.objects.all().values()
+    data_list = list(inter_data)
+    data_list = {"code": 0, "msg": "ok", "count": len(str(data_list)), "data": data_list}
+    return HttpResponse(json.dumps(data_list))
+
+
+
